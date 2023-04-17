8ca12dd352fe4d361dfdb8dbacdd96005fae3545
Rioba-Ian <riobafelix@gmail.com>
2023-04-17 16:06:04 +0300

    style on mobile screen

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 console.log("rioba");
-
-// input form
-const inputForm = document.querySelector("form");
-
-inputForm.addEventListener("change", (e) => {
-  const isValid = e.target.reportValidity();
-  e.target.setAttribute("aria-invalid", !isValid);
-});
